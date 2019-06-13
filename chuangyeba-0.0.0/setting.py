b673e6b1f16ed18a66192e9fdb6f53ed80325287
heiheisenberg <heiheisenberg559@gmail.com>
2019-06-13 10:58:23 +0800

    Signed-off-by: heiheisenberg <heiheisenberg559@gmail.com>

--- chuangyeba-0.0.0/setting.py
+++ chuangyeba-0.0.0/setting.py
@@ -1,8 +1,5 @@
-# 下载的验证码保存路径
-pic_dir = 'E:\PythonSpider\创业网课\getverifycode.jpg'
-
 # 与下载的验证码同名      
-filename    = 'getverifycode.jpg'       
+filename    = 'img.jpg'       
                  
 # 验证码类型，# 例：1004表示4位字母数字，不同类型收费不同。请准确填写，否则影响识别率。在此查询所有类型 http://www.yundama.com/price.html
 codetype    = 1004
@@ -22,6 +19,3 @@ appkey = '22cc5376925e9387a23cf797cb9ba745'
 login_number = '1608040112'
 login_password = 'abc123'
 
-# 测试版cookie登陆，这里cookie值请从浏览器复制到这
-HM_LVT = '1559530870,1559533371,1559701410,1559787551'
-JLXCKID = '25e1b01b-7a60-4e67-bcde-579ed7e7ebcc'
